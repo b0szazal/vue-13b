10080dcdfc2732094093b92751f487a5522f0247
b0szazal <zalan.szasz06@gmail.com>
2024-12-13 09:29:19 +0100

    cooked

--- vue-madarak/src/views/BirdDetailView.vue
+++ vue-madarak/src/views/BirdDetailView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+   <div  class="backAndForward" @click="backByOneID()">
+      <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#000000" ><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>
+   </div>
     <div class="madar">
         <h3>{{ madar.nev }}</h3>
         <img :src="madar.kep" :alt="madar.nev" class="img"> <br>
@@ -12,6 +15,9 @@
         </p>
         <button @click="szavazokDetail(madar.id)">Szavazok</button>
     </div>
+    <div class="backAndForward" @click="forwardByOneID()">
+      <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#000000" ><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
+    </div>
  </div>
 </template>
 
@@ -23,12 +29,38 @@ import { useRoute, useRouter } from 'vue-router';
 const birdStore = useBirdStore();
 const route= useRoute()
 const router= useRouter()
-const madar=birdStore.madarak.find(m=>m.id==route.params.id);
+let madar=birdStore.madarak.find(m=>m.id==route.params.id);
 
 const szavazokDetail = (id) =>{
     birdStore.szavazok(id);
     router.push('/');
 }
+
+const backByOneID = () =>{
+   if(madar.id!=1){
+      router.push('/birds/'+(Number(madar.id)-1));
+      madar=birdStore.madarak.find(m=>m.id==madar.id-1);
+   }
+}
+
+const forwardByOneID = () =>{
+   if(birdStore.madarak.length>madar.id){
+      router.push('/birds/'+(Number(madar.id)+1));
+      madar=birdStore.madarak.find(m=>m.id==madar.id+1);
+   }
+}
+
+
+document.addEventListener("keydown", (e)=>{
+    if(e.key=="ArrowLeft"){
+      console.log(e.key)
+        backByOneID();
+    }
+    if(e.key=="ArrowRight"){
+      console.log(e.key)
+        forwardByOneID();
+    }
+})
 </script>
 
 <style scoped>
@@ -59,5 +91,8 @@ const szavazokDetail = (id) =>{
  button:hover{
     background-color: #212529;
     color: white;
+ }
+ .backAndForward{
+   height: 100%;
  }
 </style>
