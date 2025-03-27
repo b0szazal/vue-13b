c3507b0de26484cbc2b504d8102e161ddd50db2a
Zalán Szász <zalan.szasz06@gmail.com>
2025-03-27 19:00:06 +0100

    mark boxed

--- vue-tictactoe/src/App.vue
+++ vue-tictactoe/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="container">
     <h1 class="text-center">Tic Tac Toe</h1>
-    <button onclick="Reset()" class="resetButton">Reset</button>
+    <button @click="reset()" class="resetButton">Reset</button>
     <div>
       <div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+        <div class="box" @click="markBox(1,1)" v-text="board[0][0]"></div>
+        <div class="box" @click="markBox(1,2)" v-text="board[0][1]"></div>
+        <div class="box" @click="markBox(1,3)" v-text="board[0][2]"></div>
       </div>
       <div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+        <div class="box" @click="markBox(2,1)" v-text="board[1][0]"></div>
+        <div class="box" @click="markBox(2,2)" v-text="board[1][1]"></div>
+        <div class="box" @click="markBox(2,3)" v-text="board[1][2]"></div>
       </div>
       <div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+        <div class="box" @click="markBox(3,1)" v-text="board[2][0]"></div>
+        <div class="box" @click="markBox(3,2)" v-text="board[2][1]"></div>
+        <div class="box" @click="markBox(3,3)" v-text="board[2][2]"></div>
       </div>
     </div>
     <h2 class="text-center">Eredmények</h2>
@@ -27,12 +27,24 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useTicTacToeStore } from './stores/tictactoe';
 
 const tictactoeStore = useTicTacToeStore();
+const currentPlayer = ref('X');
+const board=ref([['', '', ''], ['', '', ''], ['', '', '']])
 
-function Reset(){
+const reset=()=>{
   tictactoeStore.reset();
+  board.value=[['', '', ''], ['', '', ''], ['', '', '']]
+}
+
+const markBox=(row, column)=>{
+  if(!tictactoeStore.CheckIfAlreadyMarked(row, column)){
+    tictactoeStore.pushMark(row, column, currentPlayer.value);
+    board.value[row-1][column-1]=currentPlayer.value;
+    currentPlayer.value = currentPlayer.value=='X' ? 'O' : 'X';
+  }
 }
 </script>
 
@@ -53,7 +65,7 @@ function Reset(){
   margin: 5px;
   text-align: center;
   line-height: 100px;
-  font-size: 2em;
+  font-size: 5em;
   cursor: pointer;
 }
 .resetButton{
--- vue-tictactoe/src/stores/tictactoe.js
+++ vue-tictactoe/src/stores/tictactoe.js
@@ -3,8 +3,28 @@ import { defineStore } from 'pinia'
 
 export const useTicTacToeStore = defineStore('tictactoe', () => {
   const games= ref([])
+  const currentGame = ref([])
   const results = ref("")
 
+  const pushMark=(row, column, player)=>{
+    currentGame.value.push([row, column, player]);
+  }
 
-  return { games, results }
+  const CheckIfAlreadyMarked = (row, column)=>{
+    let isMarked = false;
+    currentGame.value.forEach((element) => {
+      if (element[0] === row && element[1] === column) {
+        isMarked = true;
+      }
+    });
+    return isMarked;
+  }
+
+  const reset=()=>{
+    games.value=[]
+    currentGame.value=[]
+    results.value=[]
+  }
+
+  return { games, currentGame, results, pushMark, CheckIfAlreadyMarked, reset }
 })
