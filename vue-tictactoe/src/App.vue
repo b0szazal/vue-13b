bb47955219e5cf3386451327b7588abc02d0ebc6
Zalán Szász <zalan.szasz06@gmail.com>
2025-03-28 13:27:08 +0100

    it works

--- vue-tictactoe/src/App.vue
+++ vue-tictactoe/src/App.vue
@@ -21,14 +21,14 @@
       </div>
     </div>
     <h2 class="text-center">Results</h2>
-    <div v-text="tictactoeStore.results.value">
+    <div v-html="tictactoeStore.results">
 
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { callWithAsyncErrorHandling, cloneVNode, ref } from 'vue';
 import { useTicTacToeStore } from './stores/tictactoe';
 
 const tictactoeStore = useTicTacToeStore();
@@ -46,10 +46,12 @@ const markBox=(row, column)=>{
     tictactoeStore.pushMark(row, column, currentPlayer.value);
     board.value[row-1][column-1]=currentPlayer.value;
     AddToLine(row, column);
-    if(CheckIfWinner(row, column)){
-      alert(currentPlayer.value+" won");
+    if(CheckIfWinner()){
+      Win(currentPlayer.value+" won");
     }
-    currentPlayer.value = currentPlayer.value=='X' ? 'O' : 'X';
+    else{
+      currentPlayer.value = currentPlayer.value=='X' ? 'O' : 'X';
+    }
   }
 }
 
@@ -74,26 +76,46 @@ const currentLines=ref({
   7: ['', 0],
   8: ['', 0]
 });
+
 const AddToLine = (row, column)=>{
   for(let i=1; i<=8; i++){
-    if(lines[i].contains([row, column])){
+    let arr=lines[i];
+    if(arr.filter(a=>a[0]==row && a[1]==column).length>0){
       if(currentLines.value[i][0]==currentPlayer.value || currentLines.value[i][0]==''){
+        currentLines.value[i][0]=currentPlayer.value;
         currentLines.value[i][1]= currentLines.value[i][1]+1 ||1;
       }
     }
   }
 }
 
-const CheckIfWinner=(row, column)=>{
-  let player=currentPlayer.value;
-  let line=lines[row*3+column];
-  for(let i=0; i<line.length; i++){
-    let [r, c]=line[i];
-    if(board.value[r-1][c-1]!=player){
-      return false;
+const CheckIfWinner=()=>{
+  for(let i=1; i<=8; i++){
+    if(currentLines.value[i][1]==3){
+      return true;
     }
   }
-  return true;
+  return false;
+}
+
+const Win=(message)=>{
+  tictactoeStore.results+=`Game `+(tictactoeStore.games.length+1)+` : `+ message + `<br>`;
+  setTimeout(()=>{
+    currentLines.value={
+      1: ['', 0],
+      2: ['', 0],
+      3: ['', 0],
+      4: ['', 0],
+      5: ['', 0],  
+      6: ['', 0],
+      7: ['', 0],
+      8: ['', 0]
+    };
+    board.value=[['', '', ''], ['', '', ''], ['', '', '']]
+    tictactoeStore.games.push(tictactoeStore.currentGame.value);
+    tictactoeStore.currentGame=[];
+    currentPlayer.value='X';
+  }, 1000);
 }
 </script>
 
